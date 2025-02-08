96e1daa83c3849c56825c88a3132433183ef1820
Suleyman <o1s.suleyman@gmail.com>
2025-02-08 11:54:39 +0400

    Correct dark mode implementation, add dropdown, fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         disableTransitionOnChange
         >
         {children}
+        <ModeToggle />
         </ThemeProvider>
       </body>
     </html>
