766ddb2a1ab8f71ddc63ff85e1dd2260da53cbc1
Suleyman <o1s.suleyman@gmail.com>
2025-02-08 23:30:36 +0400

    Update server client for type inference

--- components/todo.tsx
+++ components/todo.tsx
@@ -2,6 +2,6 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function ToDo() {
     const supabase = await createClient();
-    const { data, error } = await supabase.from("todos").select("*");
+    const { data, error } = await supabase.from("events").select("*");
     return (<></>)
 }
