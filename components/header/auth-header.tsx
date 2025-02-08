a0a13f48261471ae33519f6aa5a8d71746314fc7
Suleyman <o1s.suleyman@gmail.com>
2025-02-08 17:26:41 +0400

    Update auth header with interactive hover button instead of normal button

--- components/header/auth-header.tsx
+++ components/header/auth-header.tsx
@@ -1,13 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
-import { Button } from "../ui/button";
+import InteractiveHoverLink from "./interactive-hover-link";
 
 export default async function AuthHeader() {
     const supabase = await createClient();
     const { data } = await supabase.auth.getUser();
+    let isLoggedIn: boolean;
+    data.user ? isLoggedIn = true : isLoggedIn = false;
     return (
         <>
-            {!data.user ? (<Button asChild><Link href="/auth">Get Started</Link></Button>) : (<Button asChild><Link href="/dashboard">Dashboard</Link></Button>)}
+            <InteractiveHoverLink isLoggedIn={isLoggedIn} />
         </>
     )
 }
---
+++ components/header/interactive-hover-link.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { InteractiveHoverButton } from "../magicui/interactive-hover-button";
+
+export default function InteractiveHoverLink({isLoggedIn}: {isLoggedIn: boolean}) {
+    const router = useRouter();
+    return (
+        <InteractiveHoverButton onClick={() => router.push(isLoggedIn ? "/dashboard" : "/auth")}>{isLoggedIn ? "Dashboard" : "Get Started"}</InteractiveHoverButton>
+    )
+}
