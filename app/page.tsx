8012780472f00f72d00ca25a6b3ba5861e61995e
Suleyman <o1s.suleyman@gmail.com>
2025-02-11 14:09:57 +0400

    Clean app in preparation for events deprecation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Dashboard from "@/components/events/dashboard";
 import Home from "@/components/home";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   const supabase = await createClient();
